b733711adb5685fee02efc1cd5074704d1f49171
Jim Whimpey <jimwhimpey@me.com>
2012-02-21 19:11:33 +1000

    Everything basically done except for FTP uploading

---
+++ ftp-example.rb
@@ -0,0 +1,5 @@
+# FTP information for the uploads
+host = "example.com"
+username = "example"
+password = "example"
+path = "/example/example" # http://example.com/example/example
--- resize.rb
+++ resize.rb
@@ -1,1 +1,36 @@
-# test
+require 'mini_magick'
+require './ftp.rb'
+
+# Define sizes
+small = "500"
+large = "1200"
+
+# Define output directory
+output_dir = "/Users/jimwhimpey/Desktop/"
+
+# Grab the image
+if (ARGV[0] != nil) then
+	path = ARGV[0]
+	image = MiniMagick::Image.open(path)
+else
+	p "You must pass in an image path"
+end
+
+# Check if there's an image title option
+if (ARGV[1] != nil) then
+	title = ARGV[1].gsub(/\s/,"-")
+else
+	title = Time.now.to_i.to_s
+end
+
+# Do the large resizing (if it's wider than the large)
+if (image[:width] > large.to_i) then
+	image.resize(large)
+end
+image.write(output_dir + title + "-large." + image[:format].downcase)
+
+# Do the small resizing (if it's wider than the small)
+if (image[:width] > small.to_i) then
+	image.resize(small)
+end
+image.write(output_dir + title + "-small." + image[:format].downcase)
